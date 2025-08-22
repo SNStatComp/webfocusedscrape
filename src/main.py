749de777bd784a5c7f1d4b47c7ef2ae692ec695f
dominikblatt <d.p.blatt@cbs.nl>
2025-08-22 15:42:50 +0000

    add basic structure, requirements, config setup

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from omegaconf import OmegaConf
+from util import setup
+
+
+def main():
+    config = setup("config/config.yaml")
+
+    print("Config:")
+    print(OmegaConf.to_yaml(config))
+
+
+if __name__ == "__main__":
+    pass
---
+++ src/util/setup.py
@@ -0,0 +1,11 @@
+from omegaconf import OmegaConf
+
+
+def setup(config_path):
+    # Read config object
+    config = OmegaConf.load(config_path)
+
+    # Set OS environment variables or other stuff...
+    # ...
+
+    return config
