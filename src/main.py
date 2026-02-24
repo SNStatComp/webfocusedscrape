f57014c72306155220c9352fd0660d7488e8ea64
dominikblatt <84blatt@gmail.com>
2026-02-24 14:35:06 +0000

    improve main flow, save to parquet

--- src/main.py
+++ src/main.py
@@ -1,14 +1,30 @@
+import os
 from omegaconf import OmegaConf
 from util import setup
-import json
 import logging
 from datetime import datetime
+import pandas as pd
 
 from fetch import Fetcher
 from crawling import HesitantCrawler
 from extract import MainContentExtractor
 
 logging.basicConfig(level=logging.INFO)
+
+
+def save_batch(batch, batch_id: int, dir_out: str):
+    df = pd.DataFrame(batch)
+
+    # add partition column
+    df["batch"] = batch_id
+
+    df.to_parquet(
+        dir_out,
+        engine="pyarrow",
+        partition_cols=["batch"],
+        index=False,
+        compression="snappy"
+    )
 
 
 def main():
@@ -28,36 +44,60 @@ def main():
     with open(f"{config.input.input_dir}/{config.input.input_files.keywords}", 'r', encoding='utf-8') as file_in:
         keywords = [line.rstrip() for line in file_in]
 
-    fetcher = Fetcher()
-    for baseURL in urls:
+    # create output folder with current datetime
+    dir_out = f"{config.output.output_dir}/{datetime.now().strftime('%Y%m%d_%H%M%S')}"
+    os.makedirs(dir_out, exist_ok=True)
+    # TODO instead consider a given folder name and crash-robust resuming of batch iteration
+
+    # crawl, fetch an extract in batches
+    extractor = MainContentExtractor()
+    buffer = []
+    batch_id = 0
+
+    for base_url in urls:
+
+        print(f"Crawling base url: {base_url}")
         # crawl url
         urlCrawler = HesitantCrawler(
-            start_url=baseURL,
+            start_url=base_url,
             target_keywords=keywords,
             max_crawl_visits=100,
             add_sitemapurls=False,
             hesitancy=2
         )
 
-        urlCrawler.crawl(baseURL)
+        urlCrawler.crawl(base_url)
         crawledResults = urlCrawler.get_results()
+
+        fetcher = Fetcher()
 
         for crawledResult in crawledResults:
             fetcher.fetch(url=crawledResult["url"])
         
-    data = fetcher.get_results()
-    print("Data:", data.keys())
-    logging.info(f"#Fetcher results: {len(data.keys())} for {len(urls)} base URLs")
+        data = fetcher.get_results()
+        print("Data:", data.keys())
 
-    # TODO: add extract
-    # TODO: improve output flow, use parquet?
-    extractor = MainContentExtractor()
+        for url, html in data.items():
+            buffer.append({
+                "base_url": base_url,
+                "url": url,
+                "content": extractor.extract(html=html["HTML"])
+            })
 
-    filepath = f"{config.output.output_dir}/{datetime.now().strftime('%Y%m%d_%H%M%S')}fetched.jsonl"
-    with open(filepath, 'a', encoding='utf-8') as file_out:
-        for _url, result in data.items():
-            file_out.write(json.dumps({'URL': _url, 'HTML': result["HTML"]}))
+            if len(buffer) >= config.output.batchsize:
+                save_batch(batch=buffer, batch_id=batch_id, dir_out=dir_out)
+                buffer = []
+                batch_id += 1
+    
+    # Remaining rows at the end
+    if buffer:
+        save_batch(batch=buffer, batch_id=batch_id, dir_out=dir_out)
 
 
 if __name__ == "__main__":
     main()
+
+    # # Read the output files by using the following syntax:
+    # config = setup("../config/config.yaml")
+    # df = pd.read_parquet(f"{config.output.output_dir}/20260224_143112", engine="pyarrow")
+    # print(df.head())
