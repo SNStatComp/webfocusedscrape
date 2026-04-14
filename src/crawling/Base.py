3e132babebe66747993598098678c1b97fae435d
dominikblatt <84blatt@gmail.com>
2026-04-14 15:19:49 +0000

    add logging to Base class

--- src/crawling/Base.py
+++ src/crawling/Base.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import List
+import logging
 
 
 class ICrawler(ABC):
@@ -7,10 +8,16 @@ class ICrawler(ABC):
     Discover pages that might match target category
     """
     def __init__(self, user_agent: str, start_url: str):
-        if not start_url.startswith('https://') and not start_url.startswith('http://'):
-            start_url = f"https://{start_url}"
-        self.start_url = start_url
+
+        logging.debug(f"Crawler user agent given as: {user_agent}")
         self.user_agent = user_agent
+
+        logging.debug(f"Crawler start url given as: {start_url}")
+        if not start_url.startswith('https://') and not start_url.startswith('http://'):
+            logging.debug("Start URL lacks required http or https prefix")
+            start_url = f"https://{start_url}"
+            logging.info(f"Prefix 'https://' added to start URL: {start_url}")
+        self.start_url = start_url
 
     @abstractmethod
     def is_allowed(self, url: str) -> bool:
