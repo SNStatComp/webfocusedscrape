9e17652429a052b764a6ca1b0e9a843672365f1d
dominikblatt <84blatt@gmail.com>
2026-06-25 09:42:51 +0000

    remove unused lines

--- src/util/setup.py
+++ src/util/setup.py
@@ -4,8 +4,4 @@ from omegaconf import OmegaConf
 def setup(config_path):
     # Read config object
     config = OmegaConf.load(config_path)
-
-    # Set OS environment variables or other stuff...
-    # ...
-
     return config
